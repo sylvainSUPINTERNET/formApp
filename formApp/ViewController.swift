6955b8b6dedd96ee8c818699aa3bb98b318aeeba
SUP'Internet 15 <supinternet15@macbook-air-de-supinternet-8.local>
2018-01-22 10:51:15 +0100

    Initial Commit

---
+++ formApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  formApp
+//
+//  Created by SUP'Internet 15 on 22/01/2018.
+//  Copyright © 2018 SUP'Internet 15. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ formAppTests/formAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  formAppTests.swift
+//  formAppTests
+//
+//  Created by SUP'Internet 15 on 22/01/2018.
+//  Copyright © 2018 SUP'Internet 15. All rights reserved.
+//
+
+import XCTest
+@testable import formApp
+
+class formAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
